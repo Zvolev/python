444afbf73af6be68dd2a3d8e030307ce8f3073a4
Evgenii <zvolev@mail.ru>
2023-04-15 22:33:33 +0300

    HW_seminar2

--- lesson2/HW1.py
+++ lesson2/HW1.py
@@ -7,7 +7,7 @@ from random import randint
 которые нужно перевернуть. С клавиатуры вводятся число монет
 и сами монеты построчно. Пример: Ввод: 1 1 1 1 0 0 -> 2 """
 
-N = int(input('укажите количество монет => '))
+""" N = int(input('укажите количество монет => '))
 num_eagle = 0
 num_tails = 0
 for _ in range(N):
@@ -18,4 +18,14 @@ for _ in range(N):
         num_eagle += 1
     else:
         num_tails += 1
-print(f"Минимальное количество, которые нужно перевернуть {min(num_eagle, num_tails)}")
+print(f"Минимальное количество, которые нужно перевернуть {min(num_eagle, num_tails)}") """
+
+
+N = int(input('укажите количество монет => '))
+num_coins = 0
+for _ in range(N):
+    #  coin = int(input())
+    coin = randint(0, 1)
+    print(coin)
+    num_coins += coin
+print(f"Минимальное количество, которые нужно перевернуть {min(num_coins, N - num_coins)}")
